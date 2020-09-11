ff2587de0bdbd47f592c78d199910f04b4632314
Artiom Bozieac <artiomytprofile@gmail.com>
2020-09-11 17:40:40 +0300

    Added win text, play & stop buttons

--- Tic-Tac-Toe.cpp
+++ Tic-Tac-Toe.cpp
@@ -4,20 +4,51 @@
 using namespace std;
 using namespace sf;
 
-RenderWindow window(VideoMode(630, 830), "Tic-Tac-Toe", Style::Close | Style::Titlebar); //Creatting the window
+RenderWindow window(VideoMode(630, 830), "Tic-Tac-Toe", Style::Close | Style::Titlebar); //Creates the window
 
 class TicTacToe {
 private:
-    Sprite grid[9]; //Creatting the grid
+    Sprite grid[9]; //Creates the grid
     Sprite gameGrid;
     Texture gridTex;
     Texture X;
     Texture O;
-    Texture Nothing;
+    Texture nothing;
+    Texture button;
+    Text win;
+    Text play;
+    Text stop;
+    Font font;
+    Sprite playSprite;
+    Sprite stopSprite;
     char turn = 'X';
+    bool active = false;
 public:
     TicTacToe() { //Constructor
-        Nothing.loadFromFile("Textures/Nothing.png");
+        button.loadFromFile("Textures/button.png");
+        playSprite.setTexture(button);
+        playSprite.setOrigin(Vector2f(playSprite.getLocalBounds().width / 2, playSprite.getLocalBounds().height / 2));
+        playSprite.setPosition(166, 780);
+        stopSprite.setTexture(button);
+        stopSprite.setOrigin(Vector2f(stopSprite.getLocalBounds().width / 2, stopSprite.getLocalBounds().height / 2));
+        stopSprite.setPosition(466, 780);
+        font.loadFromFile("Fonts/myriad.OTF");
+        win.setFont(font);
+        win.setCharacterSize(55);
+        win.setFillColor(Color::Black);  
+        play.setFont(font);
+        play.setCharacterSize(40);
+        play.setFillColor(Color::Black);
+        play.setString("Play");
+        play.setOrigin(Vector2f(win.getLocalBounds().width / 2, win.getLocalBounds().height / 2));
+        play.setPosition(140, 750);
+        stop.setFont(font);
+        stop.setCharacterSize(40);
+        stop.setFillColor(Color::Black);
+        stop.setString("Stop");
+        stop.setOrigin(Vector2f(win.getLocalBounds().width / 2, win.getLocalBounds().height / 2));
+        stop.setPosition(435, 750);
+        nothing.loadFromFile("Textures/Nothing.png");
         X.loadFromFile("Textures/X.png");
         O.loadFromFile("Textures/O.png");
         gridTex.loadFromFile("Textures/Grid.png");
@@ -27,7 +58,7 @@ public:
 
         for (int i = 0; i < 9; i++) {
             grid[i].setOrigin(grid[i].getLocalBounds().width / 2, grid[i].getLocalBounds().height / 2);
-            grid[i].setTexture(Nothing);
+            grid[i].setTexture(nothing);
             grid[i].setScale(1, 1);
         }
 
@@ -51,7 +82,7 @@ public:
     }
 
     void SetCellTexture(Sprite& sprite, char& turn) { //Sets the texture for the chosen sprite
-        if (sprite.getTexture() == &Nothing) {
+        if (sprite.getTexture() == &nothing) {
             if (turn == 'X') {
                 turn = 'O';
                 sprite.setTexture(X);
@@ -63,39 +94,129 @@ public:
         }
     }
 
-    void SelectCell() {
+    void ClearGrid() { //Clears sprites' textures
+        for (int i = 0; i < 9; i++) {
+            grid[i].setTexture(nothing);
+        }
+    };
+
+    void XWin() { //Text for X won
+        win.setString("X player won");
+        win.setPosition(315, 35);
+        win.setOrigin(Vector2f(win.getLocalBounds().width / 2, win.getLocalBounds().height / 2));
+    }
+
+    void OWin() { //Text for O win
+        win.setString("O player won");
+        win.setPosition(315, 35);
+        win.setOrigin(Vector2f(win.getLocalBounds().width / 2, win.getLocalBounds().height / 2));
+    }
+
+    void CheckWin(char player) { //Checks if someone won
+        Texture* t;
+        if (player == 'X')
+            t = &X;
+        else
+            t = &O;
+
+        if ((grid[0].getTexture() == t) && (grid[1].getTexture() == t) && (grid[2].getTexture() == t)) {
+            active = false;
+            if (player == 'X')
+                XWin();
+            else
+                OWin();
+        }
+        if ((grid[3].getTexture() == t) && (grid[4].getTexture() == t) && (grid[5].getTexture() == t)) {
+            active = false;
+            if (player == 'X')
+                XWin();
+            else
+                OWin();
+        }
+        if ((grid[6].getTexture() == t) && (grid[7].getTexture() == t) && (grid[8].getTexture() == t)) {
+            active = false;
+            if (player == 'X')
+                XWin();
+            else
+                OWin();
+        }
+        if ((grid[0].getTexture() == t) && (grid[3].getTexture() == t) && (grid[6].getTexture() == t)) {
+            active = false;
+            if (player == 'X')
+                XWin();
+            else
+                OWin();
+        }
+        if ((grid[1].getTexture() == t) && (grid[4].getTexture() == t) && (grid[7].getTexture() == t)) {
+            active = false;
+            if (player == 'X')
+                XWin();
+            else
+                OWin();
+        }
+        if ((grid[2].getTexture() == t) && (grid[5].getTexture() == t) && (grid[8].getTexture() == t)) {
+            active = false;
+            if (player == 'X')
+                XWin();
+            else
+                OWin();
+        }
+        if ((grid[0].getTexture() == t) && (grid[4].getTexture() == t) && (grid[8].getTexture() == t)) {
+            active = false;
+            if (player == 'X')
+                XWin();
+            else
+                OWin();
+        }
+        if ((grid[2].getTexture() == t) && (grid[4].getTexture() == t) && (grid[6].getTexture() == t)) {
+            active = false;
+            if (player == 'X')
+                XWin();
+            else
+                OWin();
+        }
+    }
+
+    void SelectCell() { //Selects the cell
         if (CheckClick(grid[0], window)) {
             SetCellTexture(grid[0], turn);
+            CheckWin('X'); CheckWin('O');
         }
         else if (CheckClick(grid[1], window)) {
             SetCellTexture(grid[1], turn);
+            CheckWin('X'); CheckWin('O');
         }
         else if (CheckClick(grid[2], window)) {
             SetCellTexture(grid[2], turn);
+            CheckWin('X'); CheckWin('O');
         }
         else if (CheckClick(grid[3], window)) {
             SetCellTexture(grid[3], turn);
+            CheckWin('X'); CheckWin('O');
         }
         else if (CheckClick(grid[4], window)) {
             SetCellTexture(grid[4], turn);
+            CheckWin('X'); CheckWin('O');
         }
         else if (CheckClick(grid[5], window)) {
             SetCellTexture(grid[5], turn);
+            CheckWin('X'); CheckWin('O');
         }
         else if (CheckClick(grid[6], window)) {
             SetCellTexture(grid[6], turn);
+            CheckWin('X'); CheckWin('O');
         }
         else if (CheckClick(grid[7], window)) {
             SetCellTexture(grid[7], turn);
+            CheckWin('X'); CheckWin('O');
         }
         else if (CheckClick(grid[8], window)) {
             SetCellTexture(grid[8], turn);
+            CheckWin('X'); CheckWin('O');
         }
     }
 
     void Run() { //Runs the game
-        bool active = true;
-
         while (window.isOpen()) {
             Event event;
             while (window.pollEvent(event)) {
@@ -104,17 +225,32 @@ public:
                     window.close();
                     break;
                 case Event::MouseButtonPressed:
-                    if (event.mouseButton.button == Mouse::Left) {
-                        SelectCell();
-                    } 
-                    break;
+                    if (CheckClick(playSprite, window)) {
+                        ClearGrid();
+                        active = true;
+                    }
+                    else if (CheckClick(stopSprite, window)) {
+                        ClearGrid();
+                        active = false;
+                    }
+                    if (active) {
+                        if (event.mouseButton.button == Mouse::Left) {
+                            SelectCell();
+                        }
+                        break;
+                    }
                 }
             }
 
-            window.clear(Color::Cyan); //Displaying everything
+            window.clear(Color::Cyan); //Displays everything
             for (int i = 0; i < 9; i++)
                 window.draw(grid[i]);
             window.draw(gameGrid);
+            window.draw(win); 
+            window.draw(playSprite);
+            window.draw(stopSprite);
+            window.draw(play);
+            window.draw(stop);
             window.display();
         }
     }
